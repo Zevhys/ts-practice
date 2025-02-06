767e399d32692e5abcedfa8eb9a9e656cfcccdf3
Zevhys <rakha2728@gmail.com>
2025-02-06 11:39:41 +0800

    change exercise material

---
+++ 3-Union-Literal/src/index.ts
@@ -0,0 +1,57 @@
+// ! Category : Easy
+
+function printStatus(status: "success" | "error" | "loading") {
+  if (status === "success") {
+    console.log("Operation was successful.");
+  } else if (status === "error") {
+    console.log("An error occurred during the operation.");
+  } else if (status === "loading") {
+    console.log("Operation is loading.");
+  }
+}
+
+printStatus("success");
+printStatus("error");
+printStatus("loading");
+
+console.log("");
+
+// ! Category : Medium
+
+function calculateArea(
+  shape: "circle" | "rectangle",
+  dimensions: number | { width: number; height: number }
+) {
+  if (shape === "circle" && typeof dimensions === "number") {
+    return Math.PI * Math.pow(dimensions, 2);
+  } else if (shape === "rectangle" && typeof dimensions === "object") {
+    return dimensions.width * dimensions.height;
+  } else {
+    return "Invalid parameters.";
+  }
+}
+
+console.log(calculateArea("circle", 5));
+console.log(calculateArea("rectangle", { width: 4, height: 5 }));
+console.log(calculateArea("rectangle", 5));
+
+console.log("");
+
+// ! Category : Hard
+
+class Vehicle {
+  constructor(
+    public type: "car" | "motorcycle",
+    public fuel: "petrol" | "diesel" | "electric"
+  ) {}
+
+  describe() {
+    return `This vehicle is a ${this.type} that runs on ${this.fuel}.`;
+  }
+}
+
+const car = new Vehicle("car", "petrol");
+console.log(car.describe());
+
+const motorcycle = new Vehicle("motorcycle", "electric");
+console.log(motorcycle.describe());
