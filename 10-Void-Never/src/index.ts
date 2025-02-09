43fcca143355f82b45ca967fc43f85a524415309
Zevhys <rakha2728@gmail.com>
2025-02-09 20:01:16 +0800

    completed typescript exercise : void & never

---
+++ 10-Void-Never/src/index.ts
@@ -0,0 +1,24 @@
+// ! Category : Easy
+// TODO : Create a function named `logInfo` that takes a single parameter of type string and returns void. The function should print the received message to the console in the format "Info: [message]".
+
+function logInfo(params: string): void {
+  console.log(`Info: ${params}`);
+}
+
+logInfo("Application started");
+logInfo("User logged in");
+
+console.log("");
+
+// ! Category : Medium
+// TODO : Create a function named `handleError` that takes a single parameter of type string and returns never. The function should always throw an exception with the received message.
+
+function handleError(params: string): never {
+  throw new Error(params);
+}
+
+try {
+  handleError("Critical failure detected.");
+} catch (e) {
+  console.error(e);
+}
